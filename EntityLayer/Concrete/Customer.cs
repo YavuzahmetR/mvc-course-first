df1f52998025e2f9d089301f113914a51833ac17
Ahmet Yavuz Işık <95169799+YavuzahmetR@users.noreply.github.com>
2023-05-24 14:06:33 +0300

    Add files via upload
    Entity layer contains ; classes, properties also tables in SSMS

---
+++ EntityLayer/Concrete/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        public required string CustomerName { get; set; }
+
+        public required string CustomerCity { get; set; }
+
+        public int JobID { get; set; }
+
+        public Job Job { get; set;}
+    }
+}
---
+++ EntityLayer/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public required string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short UnitsInStock { get; set; }
+    }
+}
